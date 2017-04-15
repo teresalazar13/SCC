e08d40d6c4b6426f60ee02bd6e7c3f85d9bf50c1
teresalazar13 <teresa.sal13@gmail.com>
2017-04-15 11:40:42 +0100

    Adds execution_time to Simulator. Simulator stops when current_time == execution_time.

--- simulador.py
+++ simulador.py
@@ -5,12 +5,18 @@ import lista
 import eventos
 
 
+# TODO - Distinguir entre os vários serviços, com filas de espera separadas e tempos de esperaatendimento diferentes
+# TODO - Distinguir entre cliente A e B, que já foi perfurado ou polido e nao criar clientes ao calhas
+# TODO - Neste momento, o evento de Saida tira o cliente da simulação, nós queremos que este vá para o próximo serviço
+# TODO - O nr número de máquinas, tp de atendimentos e variação em cada serviço deve ser introduzido num interface graf
+
+
 class Simulador:
 
-    def insereEvento(self, event):
-        self.event_list.insert_event(event)
-
     def __init__(self):
+        # Tempo de execucao. Simulador para quando instant < execution_time
+        self.execution_time = 10000
+
         # Medias das distribuicoes de chegadas e de atendimento no servico
         self.media_cheg = 1
         self.media_serv = 1.5
@@ -23,18 +29,21 @@ class Simulador:
 
         # Servico - pode haver mais do que um num simulador
         self.client_queue = fila.Fila(self)
+
         # Lista de eventos - onde ficam registados todos os eventos que vao ocorrer na simulacao
-        # Cada simulador so tem uma
         self.event_list = lista.Lista(self)
 
-        # Agendamento da primeira chegada
-        # Se nao for feito, o simulador nao tem eventos para simular
+        # Agendamento da primeira chegada. Se nao for feito, o simulador nao tem eventos para simular
         self.insereEvento(eventos.Chegada(self.instant, self))
+
+    def insereEvento(self, event):
+        self.event_list.insert_event(event)
 
     # Metodo executivo do simulador
     def executa(self):
         # Enquanto nao atender todos os clientes
-        while self.client_queue.atendidos < self.n_clientes:
+        # TODO -> -> deve ser mudado para enquanto o tempo nao tive passado
+        while self.instant < self.execution_time:
             print(self.event_list)  # Mostra lista de eventos - desnecessario; e apenas informativo
             event = self.event_list.remove_event()  # Retira primeiro evento (e o mais iminente) da lista de eventos
             self.instant = event.instant  # Actualiza relogio de simulacao
